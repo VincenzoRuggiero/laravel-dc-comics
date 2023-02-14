be1b030d954581647175e04698eeb94b6eed0f78
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-14 17:48:17 +0100

    Use fillables in Model to store data from a form

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+
+    // Bonus 2 - Declaring fillable array with values that will be used inside
+    // ComicController's 'store' method
+    protected $fillable = array('title', 'description', 'thumb', 'price', 'series', 'sale_date', 'type');
 }
