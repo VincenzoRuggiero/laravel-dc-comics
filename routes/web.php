8ce173ae542cac14bc7d6a87c6730b7082d6a6cd
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-15 15:38:55 +0100

    Add commented route

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::get('/', [PageController::class, 'index'])->name('home');
 // Route::get('/products/create', [ComicController::class, 'create'])->name('products.create');
 // Route::post('products/store', [ComicController::class, 'store'])->name('products.store');
 // Route::get('/products/{id}', [ComicController::class, 'show'])->name('products.show');
+// Route::get('/products/{id}/edit', [ComicController::class, 'edit'])->name('products.edit');
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('products', ComicController::class);
