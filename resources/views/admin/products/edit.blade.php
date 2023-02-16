c333f0d92c930b2171aee983520b8665dbf2f635
Vincenzo Ruggiero <vincenzoru@live.it>
2023-02-16 16:53:48 +0100

    Add missing errors display message

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -9,6 +9,17 @@
             <form action="{{ route('admin.products.update', $product->id )}}" method="POST">
             @csrf
             @method('PUT')
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>
+                                {{ $error }}
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
                 <div class="mb-3">
                     <label for="title" class="form-label">Title</label>
                     <input type="text" class="form-control" name="title" value="{{ $product->title }}">
